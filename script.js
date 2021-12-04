c0dd59bb5c7b97241e82da46a3b252ca8afeafdf
Yed Star <chstokrp@gmail.com>
2021-12-04 02:08:03 -0800

    brain getting bigger but still dying

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-const letters = "abcdefghijklmnopqrstuvwxyz";
-const numbers = "0123456789";
-const symbols = "!#$%&'()*+,-./:;<=>?@[^_`{|}~";
-// var ;
+var passwordLength = window.prompt ("Enter number of characters");
+var confirmLc = window.confirm ("Use lowercase letters?");
+var confirmUc = window.confirm ("Use uppercase letters?");
+var confirmNum = window.confirm ("Use numbers?");
+var confirmSym = window.confirm ("Use symbols?");
+
+console.log(passwordLength);
+console.log(confirmLc);
+console.log(confirmUc);
+console.log(confirmNum);
+console.log(confirmSym);
+
 
 function generatePassword() {
-  window.confirm("Use lowercase letters?");
-  //if yes run genLcl and store in a variable, then run next window confirm, if no, then run next window confirm
-  // if 
-  //  function genLcl() {
-  //  }
-  // window.confirm("Use uppercase letters?");
-  //  function genUcl() {
-  //  }
-  // window.confirm("Use numbers?");
-  //  function genNum() {
-  //  }
-  // window.confirm("Use symbols?");
-  //  function genSym() { 
-  //  }
+  // run if characters is >8
+  if (passwordLength > 8) {
+  }
+  else {
+    window.alert('Password should be between 8-128 characters.');
+  }
 }
-
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-generateBtn.addEventListener("click", generatePassword)
+   // if less than 8 or more than 128 dont generate a password
+    //if all are chosen false dont generate a password
+  //run if at least one true
 
 
 
-// Add event listener to generate button
+generatePassword();
+console.log(generatePassword);
+
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
+
+//   passwordText.value = password;
+
+// }
+
+
+// // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
