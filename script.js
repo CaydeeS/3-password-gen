699dcf27cfa68ff5098f011e25da5d32d30149c4
Yed Star <chstokrp@gmail.com>
2021-12-04 01:06:46 -0800

    yikes

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-// Assignment code here
-
-
-
- function generatePassword() {
-  var lcletters = randomLetterLC();
-  var ucletters = randomLetterUC();
-  var numbers = randomNumber();
-  var symbols = randomSymbol();
-}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+const letters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!#$%&'()*+,-./:;<=>?@[^_`{|}~";
+// var ;
+
+function generatePassword() {
+  window.confirm("Use lowercase letters?");
+  //if yes run genLcl and store in a variable, then run next window confirm, if no, then run next window confirm
+  // if 
+  //  function genLcl() {
+  //  }
+  // window.confirm("Use uppercase letters?");
+  //  function genUcl() {
+  //  }
+  // window.confirm("Use numbers?");
+  //  function genNum() {
+  //  }
+  // window.confirm("Use symbols?");
+  //  function genSym() { 
+  //  }
+}
+
 
 // Write password to the #password input
 function writePassword() {
@@ -20,22 +32,9 @@ function writePassword() {
 
 }
 
+generateBtn.addEventListener("click", generatePassword)
+
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-  
-function randomLetterLC() {
-  const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  
-}
-
-function randomLetterUC() {
-
-}
-
-function randomNumber() {
-
-}
-
-function randomSymbol() {
-
-}
+// generateBtn.addEventListener("click", writePassword);
