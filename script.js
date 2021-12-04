839d982d0b33174619b45796e2733f832de6c7f9
Yed Star <chstokrp@gmail.com>
2021-12-04 03:22:22 -0800

    the probably failing failsafe function

--- script.js
+++ script.js
@@ -1,44 +1,112 @@
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+
+// Assignment code here
+
+
 var passwordLength = window.prompt ("Enter number of characters");
 var confirmLc = window.confirm ("Use lowercase letters?");
 var confirmUc = window.confirm ("Use uppercase letters?");
 var confirmNum = window.confirm ("Use numbers?");
 var confirmSym = window.confirm ("Use symbols?");
 
-console.log(passwordLength);
-console.log(confirmLc);
-console.log(confirmUc);
-console.log(confirmNum);
-console.log(confirmSym);
 
 
-function generatePassword() {
-  // run if characters is >8
-  if (passwordLength > 8) {
+
+var failsafe = function () {
+  const checkConfirm = [confirmLc, confirmUc, confirmNum, confirmSym].filter(Boolean);
+  if (checkConfirm < 1) {
+    return alert("Not enough parameters selected to run generator");
   }
-  else {
-    window.alert('Password should be between 8-128 characters.');
+  if (passwordLength < 8) {
+    return alert("Password should be between 8-128 characters.");
   }
+  if (passwordLength > 128) {
+    return alert("Password should be between 8-128 characters.");
+  }
+  console.log(checkConfirm)
+  }
+  
+failsafe ();
+
+
+
+
+
+
+// randomNumber = Math.floor(Math.random() * 10);
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
 }
-   // if less than 8 or more than 128 dont generate a password
-    //if all are chosen false dont generate a password
-  //run if at least one true
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
 
-generatePassword();
-console.log(generatePassword);
 
-// // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
 
-//   passwordText.value = password;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function generatePassword() {
+//   // run if characters is >8
+//   if (passwordLength > 8) {
+//   }
+//   else {
+//     window.alert('Password should be between 8-128 characters.');
+//   }
 // }
+//    // if less than 8 or more than 128 dont generate a password
+//     //if all are chosen false dont generate a password
+//   //run if at least one true
 
 
-// // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+
+// generatePassword();
+// console.log(generatePassword);
+
+// // // Write password to the #password input
+// // function writePassword() {
+// //   var password = generatePassword();
+// //   var passwordText = document.querySelector("#password");
+
+// //   passwordText.value = password;
+
+// // }
+
+
+// // // Add event listener to generate button
+// // generateBtn.addEventListener("click", writePassword);
